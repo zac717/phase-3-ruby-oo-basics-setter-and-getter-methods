9d559fe0b352da3eba11902dfffafe6bf44d4008
Isaac Ongera <isaacongera717@gmail.com>
2023-02-17 16:42:52 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+    # setter Method
+    def name=(name)
+        @name = name
+    end
 
+    # getter Method
+    def name
+        @name
+    end
+    
+    # setter
+    def job=(job)
+        @job = job
+    end
+
+    # getter
+    def job
+        @job
+    end
 end
