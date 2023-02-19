864d17b92e49223d346f9d1e4b2179e5592b65ea
Isaac Ongera <isaacongera717@gmail.com>
2023-02-19 09:37:16 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
+    # setter Method
+    def name=(name)
+        @name = name
+    end
+
+    # getter Method
+    def name
+        @name
+    end
+
+    # setter Method
+    def breed=(breed)
+        @breed=breed
+    end
+
+    # getter Method
+    def breed
+        @breed
+    end
 
 end
